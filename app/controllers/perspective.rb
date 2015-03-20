ff4818e795451c0ac5d0e8c16ea233c9e9d6c868
Kevin Alwell <kevalwell@gmail.com>
2015-03-20 14:40:15 -0400

    Commit Basic Skeleteon of routes and index page

--- app/controllers/perspective.rb
+++ app/controllers/perspective.rb
@@ -1,3 +1,9 @@
-get '/perspectives' do
+get '/perspective/:id' do
+  #this should show single perspective title and content & comments
+end
 
+post '/perspective/:id/new' do
+  #this will create new perspective
+  @perspective = Perspective.create(params)
+  redirect "/perspective/#{@perspective.id}"
 end
